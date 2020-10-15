0ba03c446548108ab1138e1600975fcb0d01eccc
war-art <70770404+war-art@users.noreply.github.com>
2020-10-15 07:41:46 +0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let str = String(n);
+  let arr = str.split('');
+  let reverseArray = [];
+
+  for (let i = str.length-1; i>=0; i--){
+      let a = arr [i];
+      reverseArray.push(arr[i]);
+  }
+  return parseInt(reverseArray.join(''));
 }
